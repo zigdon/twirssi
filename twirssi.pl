6339ff1d9c92a092fd03b14a86fbe3dac7a43b89
Dan Boger <zigdon@irc.peeron.com>
2009-01-08 12:46:10 -0800

    $VERSION = "1.1"; - r309 - v1.1 - multiple accounts support

--- twirssi.pl
+++ twirssi.pl
@@ -10,8 +10,8 @@ use vars qw($VERSION %IRSSI);
 use constant { DEBUG => 0 };
 
 $VERSION = "1.1";
-my ($REV) = '$Rev: 303 $' =~ /(\d+)/;
-%IRSSI   = (
+my ($REV) = '$Rev: 309 $' =~ /(\d+)/;
+%IRSSI = (
     authors     => 'Dan Boger',
     contact     => 'zigdon@gmail.com',
     name        => 'twirssi',
@@ -24,6 +24,7 @@ my ($REV) = '$Rev: 303 $' =~ /(\d+)/;
 
 my $window;
 my $twit;
+my %twits;
 my $user;
 my $poll;
 my %nicks;
@@ -44,7 +45,31 @@ sub cmd_direct {
         return;
     }
 
-    unless ( $twit->new_direct_message( { user => $target, text => $text } ) ) {
+    &cmd_direct_as( "$user $data", $server, $win );
+}
+
+sub cmd_direct_as {
+    my ( $data, $server, $win ) = @_;
+
+    unless ($twit) {
+        &notice("Not logged in!  Use /twitter_login username pass!");
+        return;
+    }
+
+    my ( $username, $target, $text ) = split ' ', $data, 3;
+    unless ( $username and $target and $text ) {
+        &notice("Usage: /dm_as <username> <nick> <message>");
+        return;
+    }
+
+    unless ( exists $twits{$username} ) {
+        &notice("Unknown username $username");
+        return;
+    }
+
+    unless ( $twits{$username}
+        ->new_direct_message( { user => $target, text => $text } ) )
+    {
         &notice("DM to $target failed");
         return;
     }
@@ -67,11 +92,35 @@ sub cmd_tweet {
         return;
     }
 
+    &cmd_tweet_as( "$user $data", $server, $win );
+}
+
+sub cmd_tweet_as {
+    my ( $data, $server, $win ) = @_;
+
+    unless ($twit) {
+        &notice("Not logged in!  Use /twitter_login username pass!");
+        return;
+    }
+
+    $data =~ s/^\s+|\s+$//;
+    my ( $username, $data ) = split ' ', $data, 2;
+
+    unless ( $username and $data ) {
+        &notice("Usage: /tweet_as <username> <update>");
+        return;
+    }
+
+    unless ( exists $twits{$username} ) {
+        &notice("Unknown username $username");
+        return;
+    }
+
     foreach my $url ( $data =~ /(https?:\/\/\S+[\w\/])/g ) {
         eval { my $short = makeashorterlink($url); $data =~ s/\Q$url/$short/g; };
     }
 
-    unless ( $twit->update($data) ) {
+    unless ( $twits{$username}->update($data) ) {
         &notice("Update failed");
         return;
     }
@@ -125,6 +174,43 @@ sub gen_cmd {
       }
 }
 
+sub cmd_switch {
+    my ( $data, $server, $win ) = @_;
+
+    $data =~ s/^\s+|\s+$//g;
+    if ( exists $twits{$data} ) {
+        &notice("Switching to $data");
+        $twit = $twits{$data};
+        $user = $data;
+    } else {
+        &notice("Unknown user $data");
+    }
+}
+
+sub cmd_logout {
+    my ( $data, $server, $win ) = @_;
+
+    $data =~ s/^\s+|\s+$//g;
+    if ( $data and exists $twits{$data} ) {
+        &notice("Logging out $data...");
+        $twits{$data}->end_session();
+        delete $twits{$data};
+    } elsif ($data) {
+        &notice("Unknown username '$data'");
+    } else {
+        &notice("Logging out $user...");
+        $twit->end_session();
+        undef $twit;
+        delete $twits{$user};
+        if ( keys %twits ) {
+            &cmd_switch( ( keys %twits )[0], $server, $win );
+        } else {
+            Irssi::timeout_remove($poll) if $poll;
+            undef $poll;
+        }
+    }
+}
+
 sub cmd_login {
     my ( $data, $server, $win ) = @_;
     my $pass;
@@ -145,6 +231,7 @@ sub cmd_login {
     }
 
     if ($twit) {
+        $twits{$user} = $twit;
         Irssi::timeout_remove($poll) if $poll;
         $poll = Irssi::timeout_add( 300 * 1000, \&get_updates, "" );
         &notice("Logged in as $user, loading friends list...");
@@ -164,20 +251,19 @@ sub load_friends {
     while (1) {
         my $friends = $twit->friends( { page => $page } );
         last unless $friends;
-        $new_friends{ $_->{screen_name} } = time
-          foreach @$friends;
+        $new_friends{ $_->{screen_name} } = time foreach @$friends;
         $page++;
         last if @$friends == 0 or $page == 10;
         $friends = $twit->friends( page => $page );
     }
 
-    foreach (keys %new_friends) {
-      next if exists $friends{$_};
-      $friends{$_} = time;
+    foreach ( keys %new_friends ) {
+        next if exists $friends{$_};
+        $friends{$_} = time;
     }
 
-    foreach (keys %friends) {
-      delete $friends{$_} unless exists $new_friends{$_};
+    foreach ( keys %friends ) {
+        delete $friends{$_} unless exists $new_friends{$_};
     }
 }
 
@@ -207,73 +293,92 @@ sub get_updates {
 
         my $new_poll = time;
 
-        print scalar localtime, " - Polling for updates" if DEBUG;
-        my $tweets = $twit->friends_timeline(
-            { since => HTTP::Date::time2str($last_poll) } )
-          || [];
-        foreach my $t ( reverse @$tweets ) {
-            my $text = decode_entities( $t->{text} );
-            $text =~ s/%/%%/g;
-            $text =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
-            my $prefix = "";
-            if (    Irssi::settings_get_bool("show_reply_context")
-                and $t->{in_reply_to_screen_name} ne $user
-                and $t->{in_reply_to_screen_name}
-                and not exists $friends{ $t->{in_reply_to_screen_name} } )
-            {
-                $nicks{ $t->{in_reply_to_screen_name} } = time;
-                my $context = $twit->show_status( $t->{in_reply_to_status_id} );
-                if ($context) {
-                    my $ctext = decode_entities( $context->{text} );
-                    $ctext =~ s/%/%%/g;
-                    $ctext =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
-                    printf $fh "[%%B\@%s%%n] %s\n",
-                      $context->{user}{screen_name}, $ctext;
-                    $prefix = "\--> ";
-                }
-            }
-            next
-              if $t->{user}{screen_name} eq $user
-                  and not Irssi::settings_get_bool("show_own_tweets");
-            printf $fh "%s[%%B\@%s%%n] %s\n", $prefix, $t->{user}{screen_name},
-              $text;
+        &do_updates( $fh, $user, $twit );
+        foreach ( keys %twits ) {
+            next if $_ eq $user;
+            &do_updates( $fh, $_, $twits{$_} );
         }
 
-        print scalar localtime, " - Polling for replies" if DEBUG;
-        $tweets =
-          $twit->replies( { since => HTTP::Date::time2str($last_poll) } )
-          || [];
-        foreach my $t ( reverse @$tweets ) {
-            next
-              if exists $friends{ $t->{user}{screen_name} };
-
-            my $text = decode_entities( $t->{text} );
-            $text =~ s/%/%%/g;
-            $text =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
-            printf $fh "[%%B\@%s%%n] %s\n", $t->{user}{screen_name}, $text;
-        }
-
-        print scalar localtime, " - Polling for DMs" if DEBUG;
-        $tweets = $twit->direct_messages(
-            { since => HTTP::Date::time2str($last_poll) } )
-          || [];
-        foreach my $t ( reverse @$tweets ) {
-            my $text = decode_entities( $t->{text} );
-            $text =~ s/%/%%/g;
-            $text =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
-            printf $fh "[%%B\@%s%%n (%%WDM%%n)] %s\n", $t->{sender_screen_name},
-              $text;
-        }
-        print scalar localtime, " - Done" if DEBUG;
         print $fh "__friends__\n";
         &load_friends;
-        foreach (sort keys %friends) {
-          print $fh "$_ $friends{$_}\n";
+        foreach ( sort keys %friends ) {
+            print $fh "$_ $friends{$_}\n";
         }
         print $fh $new_poll;
         close $fh;
         exit;
     }
+}
+
+sub do_updates {
+    my ( $fh, $username, $obj ) = @_;
+
+    print scalar localtime, " - Polling for updates for $username" if DEBUG;
+    my $tweets =
+      $obj->friends_timeline( { since => HTTP::Date::time2str($last_poll) } )
+      || [];
+    foreach my $t ( reverse @$tweets ) {
+        my $text = decode_entities( $t->{text} );
+        $text =~ s/%/%%/g;
+        $text =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
+        my $prefix = "";
+        if (    Irssi::settings_get_bool("show_reply_context")
+            and $t->{in_reply_to_screen_name} ne $username
+            and $t->{in_reply_to_screen_name}
+            and not exists $friends{ $t->{in_reply_to_screen_name} } )
+        {
+            $nicks{ $t->{in_reply_to_screen_name} } = time;
+            my $context = $obj->show_status( $t->{in_reply_to_status_id} );
+            if ($context) {
+                my $ctext = decode_entities( $context->{text} );
+                $ctext =~ s/%/%%/g;
+                $ctext =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
+                printf $fh "[%s%%B\@%s%%n] %s\n",
+                  ( $username ne $user ? "$username: " : "" ),
+                  $context->{user}{screen_name}, $ctext;
+                $prefix = "\--> ";
+            }
+        }
+        next
+          if $t->{user}{screen_name} eq $username
+              and not Irssi::settings_get_bool("show_own_tweets");
+        printf $fh "%s[%s%%B\@%s%%n] %s\n",
+          $prefix,
+          ( $username ne $user ? "$username: " : "" ),
+          $t->{user}{screen_name},
+          $text;
+    }
+
+    print scalar localtime, " - Polling for replies" if DEBUG;
+    $tweets = $obj->replies( { since => HTTP::Date::time2str($last_poll) } )
+      || [];
+    foreach my $t ( reverse @$tweets ) {
+        next
+          if exists $friends{ $t->{user}{screen_name} };
+
+        my $text = decode_entities( $t->{text} );
+        $text =~ s/%/%%/g;
+        $text =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
+        printf $fh "[%s%%B\@%s%%n] %s\n",
+          ( $username ne $user ? "$username: " : "" ),
+          $t->{user}{screen_name},
+          $text;
+    }
+
+    print scalar localtime, " - Polling for DMs" if DEBUG;
+    $tweets =
+      $obj->direct_messages( { since => HTTP::Date::time2str($last_poll) } )
+      || [];
+    foreach my $t ( reverse @$tweets ) {
+        my $text = decode_entities( $t->{text} );
+        $text =~ s/%/%%/g;
+        $text =~ s/(^|\W)\@([-\w]+)/$1%B\@$2%n/g;
+        printf $fh "[%s%%B\@%s%%n (%%WDM%%n)] %s\n",
+          ( $username ne $user ? "$username: " : "" ),
+          $t->{sender_screen_name},
+          $text;
+    }
+    print scalar localtime, " - Done" if DEBUG;
 }
 
 sub monitor_child {
@@ -284,19 +389,19 @@ sub monitor_child {
     if ( open FILE, $filename ) {
         my @lines;
         while (<FILE>) {
-          chomp;
-          last if /^__friends__/;
-          push @lines, $_ unless /^__friends__/;
+            chomp;
+            last if /^__friends__/;
+            push @lines, $_ unless /^__friends__/;
         }
 
         %friends = ();
         while (<FILE>) {
-          if (/^\d+$/) {
-            $last_poll = $_;
-            last;
-          }
-          my ($f, $t) = split ' ', $_;
-          $nicks{$f} = $friends{$f} = $t;
+            if (/^\d+$/) {
+                $last_poll = $_;
+                last;
+            }
+            my ( $f, $t ) = split ' ', $_;
+            $nicks{$f} = $friends{$f} = $t;
         }
 
         print "new last_poll = $last_poll" if DEBUG;
@@ -338,14 +443,18 @@ Irssi::settings_add_bool( "twirssi", "show_reply_context", 0 );
 Irssi::settings_add_bool( "twirssi", "show_own_tweets",    1 );
 $window = Irssi::window_find_name( Irssi::settings_get_str('twitter_window') );
 if ($window) {
-    Irssi::command_bind( "dm",            "cmd_direct" );
-    Irssi::command_bind( "tweet",         "cmd_tweet" );
-    Irssi::command_bind( "twitter_login", "cmd_login" );
+    Irssi::command_bind( "dm",             "cmd_direct" );
+    Irssi::command_bind( "tweet",          "cmd_tweet" );
+    Irssi::command_bind( "dm_as",          "cmd_direct_as" );
+    Irssi::command_bind( "tweet_as",       "cmd_tweet_as" );
+    Irssi::command_bind( "twitter_login",  "cmd_login" );
+    Irssi::command_bind( "twitter_logout", "cmd_logout" );
+    Irssi::command_bind( "twitter_switch", "cmd_switch" );
     Irssi::command_bind(
         "twirssi_version",
         sub {
             &notice(
-                "Twirssi v$VERSION (r$REV).  See details at http://tinyurl.com/twirssi"
+"Twirssi v$VERSION (r$REV).  See details at http://tinyurl.com/twirssi"
             );
         }
     );
@@ -354,7 +463,7 @@ if ($window) {
         &gen_cmd(
             "/twitter_friend <username>",
             "create_friend",
-            sub { &notice("Following $_[0]"); $nicks{$_[0]} = time; }
+            sub { &notice("Following $_[0]"); $nicks{ $_[0] } = time; }
         )
     );
     Irssi::command_bind(
@@ -362,7 +471,7 @@ if ($window) {
         &gen_cmd(
             "/twitter_unfriend <username>",
             "destroy_friend",
-            sub { &notice("Stopped following $_[0]"); delete $nicks{$_[0]}; }
+            sub { &notice("Stopped following $_[0]"); delete $nicks{ $_[0] }; }
         )
     );
     Irssi::command_bind( "twitter_updates", "get_updates" );
@@ -370,7 +479,8 @@ if ($window) {
 
     &notice("  %Y<%C(%B^%C)%N                   TWIRSSI v%R$VERSION%N (r$REV)");
     &notice("   %C(_(\\%N        http://tinyurl.com/twirssi for full docs");
-    &notice( "    %Y||%C `%N Log in with /twitter_login, send updates with /tweet");
+    &notice(
+        "    %Y||%C `%N Log in with /twitter_login, send updates with /tweet");
 
     if ( my $provider = Irssi::settings_get_str("short_url_provider") ) {
         eval "use WWW::Shorten::$provider;";
