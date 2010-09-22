912cf25a06ed335e7f44e9b7041063943a151f5d
Dan Boger <zigdon@irc.peeron.com>
2010-09-22 14:10:33 -0700

    V2.4.4

--- twirssi.pl
+++ twirssi.pl
@@ -13,7 +13,7 @@ $Data::Dumper::Indent = 1;
 
 use vars qw($VERSION %IRSSI);
 
-$VERSION = "2.4.4beta";
+$VERSION = "2.4.4";
 %IRSSI   = (
     authors     => 'Dan Boger',
     contact     => 'zigdon@gmail.com',
