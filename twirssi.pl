f6a318b7eb1e187a3bbb04657ddf68c0d027b87c
Christian Garbs <mitch@cgarbs.de>
2010-11-13 21:33:00 +0100

    show reply-to in /twitter_info
    Remember ID and username of the original message when a reply is encountered.
    
    It looks like Net::Twitter only marks replies in timelimes, not in
    searches/subscriptions, so we can only show this information on
    timeline tweets.

--- twirssi.pl
+++ twirssi.pl
@@ -336,10 +336,12 @@ sub cmd_info {
         return;
     }
 
-    my $account =   $state{__accounts}{$nick}[$id];
-    my $service =   $state{__services}{$nick}[$id];
-#    my $timestamp = $state{__created_ats}{$nick}[$id];
-    my $tweet =     $state{__tweets}{$nick}[$id];
+    my $account       = $state{__accounts}{$nick}[$id];
+    my $service       = $state{__services}{$nick}[$id];
+#    my $timestamp    = $state{__created_ats}{$nick}[$id];
+    my $tweet         = $state{__tweets}{$nick}[$id];
+    my $reply_to_id   = $state{__reply_to_ids}{$nick}[$id];
+    my $reply_to_user = $state{__reply_to_users}{$nick}[$id];
 
     &notice( [ "info" ], ",---------" );
     &notice( [ "info" ], "| nick:    $nick_orig" );
@@ -360,6 +362,10 @@ sub cmd_info {
     } else {
 	&notice( [ "info" ], "| Service: <unknown>" );
 	&notice( [ "info" ], "| URL:     <unknown>" );
+    }
+
+    if ($reply_to_id and $reply_to_user) {
+	&notice( [ "info" ], "| ReplyTo: $reply_to_user:$reply_to_id" );
     }
     &notice( [ "info" ], "`---------" );
     
@@ -1194,6 +1200,20 @@ sub load_friends {
     return ( $added, $removed );
 }
 
+sub get_reply_to {
+    # extract reply-to-information from tweets
+    my $t = shift;
+
+    if ($t->{in_reply_to_screen_name}
+	and $t->{in_reply_to_status_id}) {
+	return sprintf 'reply_to_user:%s reply_to_id:%s ',
+	    $t->{in_reply_to_screen_name},
+	    $t->{in_reply_to_status_id};
+    } else {
+	return '';
+    }
+}
+
 sub get_updates {
     print scalar localtime, " - get_updates starting" if &debug;
 
@@ -1378,8 +1398,8 @@ sub do_updates {
         next
           if $t->{user}{screen_name} eq $username
               and not $settings{own_tweets};
-        printf $fh "id:%s account:%s nick:%s type:%s %s\n",
-          $t->{id}, $username, $t->{user}{screen_name}, $reply, $text;
+        printf $fh "id:%s account:%s %snick:%s type:%s %s\n",
+          $t->{id}, $username, &get_reply_to($t), $t->{user}{screen_name}, $reply, $text;
         $new_poll_id = $t->{id} if $new_poll_id < $t->{id};
     }
     printf $fh "id:%s account:%s type:last_id timeline\n",
@@ -1410,8 +1430,8 @@ sub do_updates {
           if exists $friends{ $t->{user}{screen_name} };
 
         my $text = &get_text( $t, $obj );
-        printf $fh "id:%s account:%s nick:%s type:tweet %s\n",
-          $t->{id}, $username, $t->{user}{screen_name}, $text;
+        printf $fh "id:%s account:%s %snick:%s type:tweet %s\n",
+          $t->{id}, $username, &get_reply_to($t), $t->{user}{screen_name}, $text;
         $new_poll_id = $t->{id} if $new_poll_id < $t->{id};
     }
     printf $fh "id:%s account:%s type:last_id reply\n", $new_poll_id, $username;
@@ -1437,8 +1457,8 @@ sub do_updates {
     foreach my $t ( reverse @$tweets ) {
         my $text = decode_entities( $t->{text} );
         $text =~ s/[\n\r]/ /g;
-        printf $fh "id:%s account:%s nick:%s type:dm %s\n",
-          $t->{id}, $username, $t->{sender_screen_name}, $text;
+        printf $fh "id:%s account:%s %snick:%s type:dm %s\n",
+          $t->{id}, $username, &get_reply_to($t), $t->{sender_screen_name}, $text;
         $new_poll_id = $t->{id} if $new_poll_id < $t->{id};
     }
     printf $fh "id:%s account:%s type:last_id dm\n", $new_poll_id, $username;
@@ -1477,8 +1497,8 @@ sub do_updates {
 
             foreach my $t ( reverse @{ $search->{results} } ) {
                 my $text = &get_text( $t, $obj );
-                printf $fh "id:%s account:%s nick:%s type:search topic:%s %s\n",
-                  $t->{id}, $username, $t->{from_user}, $topic, $text;
+                printf $fh "id:%s account:%s %snick:%s type:search topic:%s %s\n",
+                  $t->{id}, $username, &get_reply_to($t), $t->{from_user}, $topic, $text;
                 $new_poll_id = $t->{id}
                   if not $new_poll_id
                       or $t->{id} < $new_poll_id;
@@ -1519,8 +1539,8 @@ sub do_updates {
 
                 my $text = &get_text( $t, $obj );
                 printf $fh
-                  "id:%s account:%s nick:%s type:search_once topic:%s %s\n",
-                  $t->{id}, $username, $t->{from_user}, $topic, $text;
+                  "id:%s account:%s %snick:%s type:search_once topic:%s %s\n",
+                  $t->{id}, $username, &get_reply_to($t), $t->{from_user}, $topic, $text;
             }
         }
     }
@@ -1580,14 +1600,14 @@ sub get_timeline {
 
             if ($context) {
                 my $ctext = &get_text( $context, $obj );
-                printf $fh "id:%s account:%s nick:%s type:tweet %s\n",
-                  $context->{id}, $username,
+                printf $fh "id:%s account:%s %snick:%s type:tweet %s\n",
+                  $context->{id}, $username, &get_reply_to($context),
                   $context->{user}{screen_name}, $ctext;
                 $reply = "reply";
             }
         }
-        printf $fh "id:%s account:%s nick:%s type:%s %s\n",
-          $t->{id}, $username, $t->{user}{screen_name}, $reply, $text;
+        printf $fh "id:%s account:%s %snick:%s type:%s %s\n",
+          $t->{id}, $username, &get_reply_to($t), $t->{user}{screen_name}, $reply, $text;
         $last_id = $t->{id} if $last_id < $t->{id};
     }
     printf $fh "id:%s account:%s type:last_id_fixreplies %s\n",
@@ -1625,7 +1645,7 @@ sub monitor_child {
             my $hilight = 0;
             my %meta;
 
-            foreach my $key (qw/id account nick type topic/) {
+            foreach my $key (qw/id account reply_to_user reply_to_id nick type topic/) {
                 if (s/^$key:((?:\S|\\ )+)\s*//) {
                     $meta{$key} = $1;
                     $meta{$key} =~ s/%20/ /g;
@@ -1635,7 +1655,7 @@ sub monitor_child {
             # avoid internal breakage by sneaky nicknames
 	    # to be added: created_ats
             next if ($meta{nick} and $meta{nick} =~ 
-              /^__(indexes|windows|searches|fixreplies|tweets|last_tweet|last_id|accounts|services)$/);
+              /^__(indexes|windows|searches|fixreplies|tweets|last_tweet|last_id|accounts|services|reply_to_users|reply_to_ids)$/);
 
             if ( $meta{type} and $meta{type} eq 'fix_replies_index' ) {
                 $fix_replies_index{ $meta{account} } = $meta{id};
@@ -1673,9 +1693,9 @@ sub monitor_child {
                 $state{ lc $meta{nick} }[$marker]           = $meta{id};
                 $state{__indexes}{ $meta{nick} }            = $marker;
                 $state{__tweets}{ lc $meta{nick} }[$marker] = $_;
-                $state{__accounts}{ lc $meta{nick} }[$marker] = $meta{account};
-                $state{__services}{ lc $meta{nick} }[$marker] = $meta{service};
-#                $state{__created_ats}{ lc $meta{nick} }[$marker] = $meta{created_at};
+                foreach my $key (qw/account service reply_to_id reply_to_user/) { # created_at
+                    $state{"__${key}s"}{ lc $meta{nick} }[$marker] = $meta{$key};
+	        }
                 $marker                                     = ":$marker";
             }
 
